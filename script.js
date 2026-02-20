9a7caf306946c62c21169fd7d7b242a620a3252d
EfficoSaneamento <planejamento@effico.com.br>
2026-02-20 20:05:30 +0000

    ajuste

--- script.js
+++ script.js
@@ -1,45 +1,66 @@
-const URL_API = 'https://script.google.com/macros/s/AKfycbzmLvDrB8jTm-6pzBVBVNdAtwclN6ybaaUkwiai_HsrwgVkuaTy4l5KWKYY32CoK0kJ/exec';
+const API = 'https://script.google.com/macros/s/AKfycbzF7Tv37LlMN-na6J_1f-rq90axU-tVnFQCG9qDZmh0I7qdCYmGgTqQlBLpoahpin9P/exec'
 
-function carregarDados() {
-    const corpo = document.getElementById('corpoTabela');
+async function carregarSolicitacoes() {
+  const res = await fetch(`${API}?acao=listar`)
+  const dados = await res.json()
 
-    corpo.innerHTML = `
-        <tr>
-            <td colspan="8" class="p-4 text-center text-gray-400">
-                Carregando...
-            </td>
-        </tr>
-    `;
+  const tbody = document.getElementById('tabelaSolicitacoes')
+  tbody.innerHTML = ''
 
-    fetch(URL_API)
-        .then(res => res.json())
-        .then(dados => {
-            corpo.innerHTML = '';
+  dados.forEach(s => {
+    const cor =
+      s.status === 'Concluído' ? 'bg-green-200' :
+      s.status === 'Em andamento' ? 'bg-yellow-200' :
+      'bg-red-200'
 
-            dados.forEach(l => {
-                corpo.innerHTML += `
-                    <tr class="border-t">
-                        <td class="p-3">${l.IDENTIFICADOR}</td>
-                        <td class="p-3">${l['Data da Solicitação']}</td>
-                        <td class="p-3">${l['Data Limite']}</td>
-                        <td class="p-3">${l['Centro de Custo']}</td>
-                        <td class="p-3">${l.Item}</td>
-                        <td class="p-3">${l.Observacao}</td>
-                        <td class="p-3">${l.Quantidade}</td>
-                        <td class="p-3">${l.Solicitante}</td>
-                    </tr>
-                `;
-            });
-        })
-        .catch(() => {
-            corpo.innerHTML = `
-                <tr>
-                    <td colspan="8" class="p-4 text-center text-red-500">
-                        Erro ao carregar dados
-                    </td>
-                </tr>
-            `;
-        });
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
+      <td>${s.id}</td>
+      <td>${s.data}</td>
+      <td>${s.item}</td>
+      <td>${s.quantidade}</td>
+      <td>${s.comprador}</td>
+      <td><span class="px-2 py-1 rounded ${cor}">${s.status}</span></td>
+      <td>
+        <button onclick="concluir('${s.id}')"
+                class="text-green-600 font-bold">
+          Concluir
+        </button>
+      </td>
+    `
+    tbody.appendChild(tr)
+  })
 }
 
-carregarDados();
+async function concluir(id) {
+  await fetch(`${API}?acao=concluir&id=${id}`)
+  carregarSolicitacoes()
+}
+
+async function salvarAvaliacao(dados) {
+  const params = new URLSearchParams(dados).toString()
+  await fetch(`${API}?acao=avaliar&${params}`)
+}
+
+async function loginDashboard() {
+  const usuario = 'PEDRO'
+  const senha = document.getElementById('senhaDashboard').value
+
+  const res = await fetch(`${API}?acao=login&usuario=${usuario}&senha=${senha}`)
+  const r = await res.json()
+
+  if (r.acesso) {
+    carregarDashboard()
+  } else {
+    alert('Senha inválida')
+  }
+}
+
+async function carregarDashboard() {
+  const res = await fetch(`${API}?acao=dashboard`)
+  const d = await res.json()
+
+  document.getElementById('dashPendentes').innerText = d.pendente
+  document.getElementById('dashAndamento').innerText = d.andamento
+  document.getElementById('dashConcluidas').innerText = d.concluido
+}
